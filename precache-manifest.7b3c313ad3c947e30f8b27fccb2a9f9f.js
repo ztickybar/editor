bdcf25edbcbd16656e7fbb9a904b35fa623db92a
NielsGregers <niels@jumpto365.com>
2020-02-12 15:41:41 +0100

    Updates

--- precache-manifest.7b3c313ad3c947e30f8b27fccb2a9f9f.js
+++ precache-manifest.7b3c313ad3c947e30f8b27fccb2a9f9f.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8b096fe74cd9b7cc9bb8b15560c65e91",
+    "revision": "2b2d5ddcdd8bac70fef05a5db0b16c86",
     "url": "/index.html"
   },
   {
-    "revision": "432442aa381e2346249c",
+    "revision": "1ba9fdbe04a11ed70ecc",
     "url": "/static/css/main.d1b05096.chunk.css"
   },
   {
-    "revision": "2018bcbb94511393a397",
-    "url": "/static/js/2.7935ca30.chunk.js"
+    "revision": "10b3e8a123fd87d8f56b",
+    "url": "/static/js/2.7b4094ff.chunk.js"
   },
   {
-    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
-    "url": "/static/js/2.7935ca30.chunk.js.LICENSE.txt"
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/static/js/2.7b4094ff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "432442aa381e2346249c",
-    "url": "/static/js/main.79524ff4.chunk.js"
+    "revision": "1ba9fdbe04a11ed70ecc",
+    "url": "/static/js/main.874810f1.chunk.js"
   },
   {
     "revision": "39385c64633a353dc31b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.34e17067e7078b4197ab0ef6398f5475.js"
+  "/precache-manifest.7b3c313ad3c947e30f8b27fccb2a9f9f.js"
 );
 
 self.addEventListener('message', (event) => {
